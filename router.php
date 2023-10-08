dd8d2bb2b196e6601ec389a0413f9c64a3f78498
devsug <v.shibaev98@gmail.com>
2023-10-08 18:58:47 +0300

    Разнес по методам логические куски, добавил доку к методам

--- router.php
+++ router.php
@@ -4,67 +4,82 @@ use Controllers\{HomePageController, NotFoundController};
 
 class Router
 {
+    /** @var string Класс контроллера, который мы создаем */
     public $controllerClass;
+    /** @var string Вызываемый метод */
     public $method;
+    /** @var array|string  */
+    public $data = [];
 
+    /** Регулярное выражение, которое мы должны заменять */
     const VAR_PATTERN = '/\{.*\}/ui';
+    /** Регулярное выражение, на которое мы должны заменять: все доступные значения */
     const URI_REPLACEMENT_ALL = '([A-Za-z0-9].*)';
+    /** Маршруты для проверки */
     const ROUTE_URLS = [
         '/' => [HomePageController::class, 'index'],
         '/student/{id}/{bebra}' => [HomePageController::class, 'getConcreteUser']
     ];
 
-    public function __construct($uri)
+    /**
+     * Конструктор класса
+     *
+     * @author Valery Shibaev
+     * @version 1.0, 08.10.2023
+     *
+     * @param string $uri Проверяемый URI
+     */
+    public function __construct(string $uri)
     {
-        [$this->controllerClass, $this->method] = $this->parseUriToSchema($uri);
+        $parsedUri = $this->parseUriToSchema($uri);
+        [$this->controllerClass, $this->method] = $parsedUri;
+        if ($parsedUri['data']) {
+            $this->data = $parsedUri['data'];
+        }
     }
 
-    private function getMethodByUri(string $uri): array
-    {
-        return self::ROUTE_URLS[$uri] ?? [NotFoundController::class, 'index'];
-    }
-
+    /**
+     * По передаваемомум uri сравнивает с роутером и парсит в переменные параметры
+     *
+     * @author Valery Shibaev
+     * @version 1.0, 08.10.2023
+     *
+     * @param string $uri Проверяемый URI
+     * @return array
+     */
     private function parseUriToSchema(string $uri): array
     {
         if (!$uri) {
             return [NotFoundController::class, 'index'];
         }
 
-        $routePatterns = array_keys(self::ROUTE_URLS);
-
-        foreach (self::ROUTE_URLS as $routePattern => $data) {
-            $parsedExplodedUri = [];
+        foreach (self::ROUTE_URLS as $routePattern => $controllerData) {
             $explodedUri = explode('/', $routePattern);
-            $varKeys = [];
 
-            if (preg_match(self::VAR_PATTERN, $routePattern)) {
-                foreach ($explodedUri as $elem) {
-                    if (preg_match(self::VAR_PATTERN, $elem)) {
-                        $elem = preg_filter(self::VAR_PATTERN, self::URI_REPLACEMENT_ALL, $elem);
-                    }
+            $parsedExplodedUri = $this->getParsedExplodedUri($routePattern, (array) $explodedUri);
 
-                    $parsedExplodedUri[] = $elem;
-                }
-            }
-
-            $parsedUri = implode('\/', $parsedExplodedUri);
+            $parsedUri = implode('\/', $parsedExplodedUri ?: $explodedUri);
             $parsedUri = '/' . $parsedUri . '/ui';
+
             if (preg_match($parsedUri, $uri) && !preg_filter($parsedUri, '', $uri)) {
-                $varKeys = array_diff(explode('/', $routePattern), explode('/', $uri));
-                $varsFromUri = [];
+                $varsFromUri = $this->getVarsFromUri($routePattern, $uri);
 
-                foreach ($varKeys as $index => $key) {
-                    $a = str_replace('}', '', str_replace('{', '', $key));
-                    $varsFromUri[$a] = explode('/', $uri)[$index];
-                }
-
-                return $data;
+                return array_merge($controllerData, ['data' => $varsFromUri]);
             }
         }
 
         return [NotFoundController::class, 'index'];
     }
 
+    /**
+     * Отдает роутер по URI
+     *
+     * @author Valery Shibaev
+     * @version 1.0, 08.10.2023
+     *
+     * @param string $uri Проверяемый URI
+     * @return Router|null
+     */
     public static function getRouter(string $uri): ?Router
     {
         if (!$uri) {
@@ -72,6 +87,60 @@ class Router
         }
 
         return new self($uri);
+    }
+
+    /**
+     * Заменяет переменные в ссылке на регулярные выражения
+     *
+     * @author Valery Shibaev
+     * @version 1.0, 08.10.2023
+     *
+     * @param string $routePattern Паттерн по которому проверяем подходит ли URI
+     * @param array $explodedUri Ссылка разбитая на массив
+     * @return array
+     */
+    protected function getParsedExplodedUri(string $routePattern, array $explodedUri): array
+    {
+        if (!$explodedUri) {
+            return [];
+        }
+
+        $parsedExplodedUri = [];
+
+        if (preg_match(self::VAR_PATTERN, $routePattern)) {
+            foreach ($explodedUri as $elem) {
+                if (preg_match(self::VAR_PATTERN, $elem)) {
+                    $elem = preg_filter(self::VAR_PATTERN, self::URI_REPLACEMENT_ALL, $elem);
+                }
+
+                $parsedExplodedUri[] = $elem;
+            }
+        }
+
+        return $parsedExplodedUri;
+    }
+
+    /**
+     * Формирует данные из переменных
+     *
+     * @author Valery Shibaev
+     * @version 1.0, 08.10.2023
+     *
+     * @param string $routePattern Паттерн, по которому мы сравниваем URI
+     * @param string $uri Проверяемый URI
+     * @return array
+     */
+    protected function getVarsFromUri(string $routePattern, string $uri): array
+    {
+        $varKeys = array_diff(explode('/', $routePattern), explode('/', $uri));
+        $varsFromUri = [];
+
+        foreach ($varKeys as $index => $key) {
+            $clearedKey = str_replace('}', '', str_replace('{', '', $key));
+            $varsFromUri[$clearedKey] = explode('/', $uri)[$index];
+        }
+
+        return $varsFromUri;
     }
 
 }
